a5b9a15dc73f8e853478f3603c50e180bb6029ef
umang <umangjitsingh@gmail.com>
2025-06-04 19:38:25 -0400

    vercel json changed

--- server.js
+++ server.js
@@ -54,7 +54,6 @@ const dbConnect = async () => {
 }
 
 
-
 dbConnect().then(()=>{
 	app.listen(PORT, () => {
 		console.log(`Server is running on port ${PORT}..`)
@@ -64,57 +63,3 @@ dbConnect().then(()=>{
 })
 
 
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-// const path = require('path');
-// const cors = require('cors');
-//
-// dotenv.config();
-//
-// const PORT = process.env.PORT;
-//
-// const app = express();
-//
-// if (process.env.NODE_ENV === 'local') {
-// 	app.use(cors({
-// 		origin: 'http://localhost:3000/',
-// 		credentials: true
-// 	}));
-// } else {
-// 	app.use(cors({
-// 		credentials: true
-// 	}));
-// }
-//
-//
-//
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static(path.join(__dirname, "./frontend/dist")));
-// 	app.get('*', (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "./", "frontend", "dist", "index.html"));
-// 	});
-// }
-//
-// const dbConnect = async () => {
-// 	try {
-// 		if (process.env.NODE_ENV === 'local') {
-// 			await mongoose.connect(process.env.LOCAL_DB_URI);
-// 			console.log('Local db connected');
-// 		} else {
-// 			await mongoose.connect(process.env.MONGODB_URI);
-// 			console.log('Production db connected');
-// 		}
-// 	} catch (e) {
-// 		console.log("FAILED DB CONNECTION... ", e);
-// 		process.exit(1);
-// 	}
-// };
-//
-// dbConnect().then(() => {
-// 	app.listen(PORT, () => {
-// 		console.log(`Server is running on port ${PORT}..`);
-// 	});
-// }).catch((e) => {
-// 	console.log("Error : error running 1.db  2.server", e);
-// });
