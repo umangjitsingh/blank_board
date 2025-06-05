05dbee356691b56029d2f582a2d947a350dd0479
umang <umangjitsingh@gmail.com>
2025-06-05 18:30:48 -0400

    u

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 if (process.env.NODE_ENV === 'production') {
 
 	app.use(express.static(path.join(__dirname,"./frontend/dist")));
-	app.get('/*', (req, res) => {
+	app.get('*', (req, res) => {
 		res.sendFile((path.resolve(__dirname,"./","frontend","dist","index.html")));
 	});
 }
